ab856baa80541cf8629fe8bbaa7715ab2cbf041e
Brian Zimmerman <briangzimmerman@gmail.com>
2017-09-21 16:31:42 -0500

    added some basic server behavior

--- server/server.js
+++ server/server.js
@@ -2,3 +2,28 @@ let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+let port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save()
+        .then((doc) => {
+            res.send(doc);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Started express server on port ${port}...`);
+})
