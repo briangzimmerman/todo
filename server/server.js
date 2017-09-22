55d56819128496af38299a7edeaf7a08378cd0c1
Brian Zimmerman <briangzimmerman@gmail.com>
2017-09-22 16:45:41 -0500

    added todo/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
+let {ObjectID} = require('mongodb');
 let express = require('express');
 let bodyParser = require('body-parser');
 
@@ -16,8 +17,8 @@ app.post('/todos', (req, res) => {
     });
 
     todo.save()
-        .then((doc) => {
-            res.send(doc);
+        .then((todo) => {
+            res.send({todo});
         })
         .catch((err) => {
             res.status(400).send(err);
@@ -28,6 +29,21 @@ app.get('/todos', (req, res) => {
     Todo.find()
         .then((todos) => res.send({todos}))
         .catch((err) => res.status(400).send(err));
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    } else {
+        Todo.findById(id)
+            .then((todo) => {
+                todo ? res.send({todo}) : res.status(404).send();
+            })
+            .catch((err) => {
+                res.status(400).send();
+            })
+    }
 });
 
 app.listen(port, () => {
