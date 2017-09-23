1f3e3e451f4a937d89dfb22b16e95aedfad311d2
Brian Zimmerman <briangzimmerman@gmail.com>
2017-09-22 19:39:51 -0500

    changed password

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/Todoapp');
-mongoose.connect('mongodb://<todoApp>:<S0meth1ngL0ngandC0mpl1cated>@ds147864.mlab.com:47864/todo-app');
+mongoose.connect('mongodb://<todoApp>:<S0meth1ngL0ngandC0mpl1cated!>@ds147864.mlab.com:47864/todo-app');
 
 module.exports = {
     mongoose
