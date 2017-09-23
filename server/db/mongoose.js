450d9251cf2a3ed89f371457fa59b0212ac455ef
Brian Zimmerman <briangzimmerman@gmail.com>
2017-09-22 19:38:07 -0500

    changed to mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Todoapp');
+// mongoose.connect('mongodb://localhost:27017/Todoapp');
+mongoose.connect('mongodb://<todoApp>:<S0meth1ngL0ngandC0mpl1cated>@ds147864.mlab.com:47864/todo-app');
 
 module.exports = {
     mongoose
